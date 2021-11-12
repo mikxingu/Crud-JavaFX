63d6f7982ce05dd882e31507490cd1dffa47a767
mikxingu <michelalvs@gmail.com>
2021-11-11 23:11:53 -0300

    Added textField event listeners and constraints

--- javafx1/src/gui/ViewController.java
+++ javafx1/src/gui/ViewController.java
@@ -1,15 +1,19 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable{
 	
 	@FXML
 	private TextField txtFirstNumber;
@@ -48,5 +52,14 @@ public class ViewController {
 	@FXML
 	public void onQuitAction() {
 		System.exit(0);
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtFirstNumber);
+		Constraints.setTextFieldDouble(txtSecondNumber);
+		Constraints.setTextFieldMaxLength(txtFirstNumber, 10);
+		Constraints.setTextFieldMaxLength(txtSecondNumber, 10);
+		
 	}
 }
