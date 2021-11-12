8d2ef2934439b3fba6e64d75d6ac8ef98a3ba0a0
mikxingu <michelalvs@gmail.com>
2021-11-11 22:44:48 -0300

    Created SUM function with NumberFormatException Handling

--- javafx1/src/gui/ViewController.java
+++ javafx1/src/gui/ViewController.java
@@ -1,26 +1,48 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 	
-	private String alertTitle = "Alert";
-	private String alertHeader = "Default";
-	private String alertContent = "I am working";
+	@FXML
+	private TextField txtFirstNumber;
 	
+	@FXML
+	private TextField txtSecondNumber;
+	
+	@FXML
+	private Label resultLabel;
+	
+	
+	@FXML
+	private Button sumButton;
 	
 	@FXML
 	private Button quitButton;
 	
 	@FXML
-	private Button button;
+	public void onSumButtomAction() {
+		
+		try {
+			Locale.setDefault(Locale.US);
+			
+			double value1 = Double.parseDouble(txtFirstNumber.getText());
+			double value2 = Double.parseDouble(txtSecondNumber.getText());
+			
+			double sum = value1 + value2;
+			
+			resultLabel.setText(String.format("%.2f", sum));
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse Error", e.getMessage(), AlertType.ERROR);
+		}
 	
-	@FXML
-	public void onButtonAction() {
-		Alerts.showAlert(alertTitle, alertHeader, alertContent, AlertType.INFORMATION);
 	}
 	
 	@FXML
